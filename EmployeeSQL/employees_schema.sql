8e309f36ef4f5ef33559c27bc2ec9a78efbfdd2d
Jayplect <evansja.femi@gmail.com>
2023-05-15 16:58:17 -0400

    made alterations to key contraints

--- EmployeeSQL/employees_schema.sql
+++ EmployeeSQL/employees_schema.sql
@@ -19,30 +19,28 @@ DROP TABLE IF EXISTS titles CASCADE;
 -- Create departments table
 CREATE TABLE departments(
 	dept_no VARCHAR(10) UNIQUE NOT NULL,
-	dept_name VARCHAR (60) NOT NULL,
+	dept_name VARCHAR (60) UNIQUE NOT NULL,
 	PRIMARY KEY (dept_no)
 	);
 	
 -- Create title table
-CREATE TABLE "titles" (
-    "title_id" VARCHAR(10)   NOT NULL,
-    "title" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
-);	
+CREATE TABLE titles (
+    "title_id" VARCHAR(10) UNIQUE NOT NULL,
+    "title" VARCHAR (30) UNIQUE NOT NULL,
+    PRIMARY KEY (title_id)
+	);	
+	
 -- Create employees table
 CREATE TABLE employees(
-	emp_no INTEGER NOT NULL,
+	emp_no INTEGER UNIQUE NOT NULL,
 	emp_title_id VARCHAR(10) NOT NULL,
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
 	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
+	first_name VARCHAR (50) NOT NULL,
+	last_name VARCHAR (50) NOT NULL,
 	sex char(1) NOT NULL,
 	hire_date DATE NOT NULL,
-	CONSTRAINT "pk_employees" PRIMARY KEY(
-        "emp_no")
+	PRIMARY KEY(emp_no)
 	);
 
 -- Create department employees table
@@ -50,8 +48,7 @@ CREATE TABLE dept_emp(
 	emp_no INTEGER NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	dept_no VARCHAR (10) NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	PRIMARY KEY (emp_no,dept_no)
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 	);
 
 -- Create department manager table
@@ -59,19 +56,17 @@ CREATE TABLE dept_manager(
 	dept_no VARCHAR (10) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	emp_no INTEGER NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY (dept_no, emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 	);
 
 -- Create salaries table
 CREATE TABLE salaries(
-	emp_no INTEGER NOT NULL,
+	emp_no INTEGER UNIQUE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY (emp_no),
 	salary INTEGER NOT NULL
 	);
 	
--- Display each table
+-- Display each table to ensure tables were created successfully
 SELECT * FROM departments;
 SELECT * FROM dept_emp;
 SELECT * FROM titles;
